2425e54395902b940b8ea400f6e357aab0fe74e1
jeepo02 <jeeplove_love@hotmail.com>
2016-06-23 13:31:16 +0700

    login
    login

---
+++ login1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Leave Online Management</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link  href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="topPan"><a href="#"></a>
+
+</div>
+<div id="headerPan">
+  <div id="headerPanleft">
+   <div id="leftmemberPan">
+   <h2>member <span>login</span></h2>
+      <form name="login" id="login"  action="check_login.php" method="post">
+        <p>
+          <label>USERNAME</label>
+          <input  name="txtUsername" type="text" id="txtUsername" />
+          <label>PASSWORD</label>
+          <input name="txtPassword" type="password" id="txtPassword">
+        </p>
+        <p>&nbsp;</p>
+       <center> 
+         <a href="admin/addprofile_admin1.php">register</a>
+       </center>
+        <div id="leftPango">
+          <p class="textposition">
+        
+            <input type="submit" class="gobutton"  value="login" /> 
+           
+          </p>
+        </div>
+      </form>
+    </div>
+    
+  </div>
+</div>
+
+</body>
+</html>
